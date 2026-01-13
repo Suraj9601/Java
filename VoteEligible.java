43803a3e208ddfae344ad0694c9fe9ecf41abba9
Suraj Aglave <aglavesuraj96@gmail.com>
2026-01-13 21:40:16 +0530

    .

---
+++ VoteEligible.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class VoteEligible {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age : ");
+        int age = sc.nextInt();
+
+        if(age>= 18){
+            System.out.println("Eligible for Vote.");
+        } else {
+            System.out.println("Not Eligible for Vote.");
+        }
+
+        sc.close();
+    }
+}
