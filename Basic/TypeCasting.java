36cce7901a8dafbf0e8663ceb1b5d102ad44b056
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-08 12:52:24 +0530

    Create TypeCasting.java

---
+++ Basic/TypeCasting.java
@@ -0,0 +1,11 @@
+class TypeCasting {
+    public static void main(String[] args){
+        byte b = 125;
+        short s = 0;
+
+        // Type casting
+
+          int c = s;// byte to int
+        System.out.println(b);
+    }
+}
