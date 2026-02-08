5b6db1182f1145cac1996e55067316f26e4cdaa4
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-08 12:52:22 +0530

    Create TypeConversion.java

---
+++ Basic/TypeConversion.java
@@ -0,0 +1,11 @@
+class TypeConversion {
+    public static void main(String[] args) {
+        byte b = 25;
+        short s = 150;
+        int i = 250;
+        long l = 1500000l;
+
+        System.out.println();
+
+    }
+}
