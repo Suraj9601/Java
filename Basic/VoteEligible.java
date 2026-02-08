04eda043558b979719994e69e8705ebea09d6bf2
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-08 13:08:45 +0530

    Create VoteEligible.java

---
+++ Basic/VoteEligible.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class VoteEligible {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age : ");
+        int age = sc.nextInt();
+
+        if(age>=18){
+            System.out.println("Eligible for Voting.");
+        } else {
+            System.out.println("Not eligible for Voting!");
+            System.out.println("Your are eligible after 18 for voting.");
+        }
+    }
+}
