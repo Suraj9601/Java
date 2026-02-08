28ae93fd1ee3e754d83ed000fac1bc06a8d8d91e
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-08 13:18:50 +0530

    Create GretestNo.java

---
+++ Basic/GretestNo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class GreatestNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first value  : ");
+        int a = sc.nextInt();
+        System.out.print("Enter second value : ");
+        int b = sc.nextInt();
+        System.out.print("Enter third value : ");
+        int c = sc.nextInt();
+
+        if(a>b || a>c) {
+            System.out.println(a + " is greater.");
+        } else if (b>a || b > c) {
+            System.out.println(b + " is greater.");
+        } else {
+            System.out.println(c + " is greater.");
+        }
+    }
+}
