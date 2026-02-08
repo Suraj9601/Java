0a672227fdfd8f0e4d3cdc576c12a99ecdf10ee9
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-08 12:52:14 +0530

    Create Basics.java

---
+++ Basic/Basics.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Basics {
+    public static void main(String[] args) {
+
+        char letter = 'A';
+        System.out.println("Character : " + letter);
+
+        String str = "Hello";
+        System.out.println("String : " + str);
+    }
+}
