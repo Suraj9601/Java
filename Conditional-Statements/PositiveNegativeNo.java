0868b6c63627e6a3a9dd6bf9d848d4109c3e77dc
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-09 20:03:37 +0530

    Create PositiveNegativeNo.java

---
+++ Conditional-Statements/PositiveNegativeNo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class PositiveNegativeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+
+        sc.close();
+
+        if (num == 0) {
+            System.out.println("Entered number is Zero.");
+        } 
+        else if (num > 0) {
+            System.out.println("Entered number id positve.");
+        } 
+        else if (num < 0) {
+            System.out.println("Entered number is negative.");
+        } 
+        else {
+            System.out.println("Please enter valid number.");
+        }
+
+    }
+}
