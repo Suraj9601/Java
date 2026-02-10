028da63551dee3da2002d3c4a4a8d102206176d9
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-10 13:42:05 +0530

    .

---
+++ Conditional-Statements/LeafYear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeafYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the year : ");
+        int year = sc.nextInt();
+        sc.close();
+
+        if (year % 4 == 0) {
+            System.out.println("Entered year is leaf year.");
+        } else {
+            System.out.println("Entered year is not leaf year.");
+        }
+    }
+}
