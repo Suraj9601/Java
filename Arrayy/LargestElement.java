ca452fa42688004c1a4f507e41fd960408c1ac30
Suraj Aglave <aglavesuraj96@gmail.com>
2026-02-14 12:35:43 +0530

    Create LargestElement.java

---
+++ Arrayy/LargestElement.java
@@ -0,0 +1,13 @@
+public class LargestElement {
+    public static void main(String[] args) {
+        int arr[] = { 2, 5, 1, 25, 10, 8, 23, 7 };
+        int result = 0;
+
+        for (int ele : arr) {
+            if (result < ele) {
+                result = ele;
+            }
+        }
+        System.out.println("Largest element : " + result);
+    }
+}
