d3c02847e500f6d602f3296f195024228f66f5ef
Suraj Aglave <aglavesuraj96@gmail.com>
2026-03-01 20:38:09 +0530

    Create RemoveDuplicates.java

---
+++ Array/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,8,2,1,4,7,3};
+        duplicates(arr);
+    }
+
+    static void duplicates(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int  i : arr) {
+            set.add(i);
+        }
+        System.out.println(set);
+    }
+}
