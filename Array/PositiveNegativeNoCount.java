73d315f042bd3713cfac1acbbc065dad014d7587
Suraj Aglave <aglavesuraj96@gmail.com>
2026-03-02 02:05:41 +0530

    -

--- Array/PositiveNegativeNoCount.java
+++ Array/PositiveNegativeNoCount.java
@@ -2,7 +2,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class PositiveNegativeNo {
+public class PositiveNegativeNoCount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[5];
