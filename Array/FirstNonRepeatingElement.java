5b838435a1ad2232f066a0afa4a9be5ae673aa5a
Suraj Aglave <aglavesuraj96@gmail.com>
2026-03-02 20:30:15 +0530

    -

---
+++ Array/FirstNonRepeatingElement.java
@@ -0,0 +1,17 @@
+public class FirstNonRepeatingElement {
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 2, 6,1, 7 ,3, 2, 6 };
+        nonRepeatingElement(arr);
+    }
+
+    static void nonRepeatingElement(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for(int j =i+1; j<arr.length; j++) {
+                if (arr[i]!=arr[j]) {
+                    System.out.println("First non repeated element : "+arr[i]);
+                    return;
+                }
+            }
+        }
+    }
+}
--- Array/FirstRepeatingElement.java
+++ Array/FirstRepeatingElement.java
@@ -1,20 +1,21 @@
-
+import java.util.HashSet;
 
 public class FirstRepeatingElement {
     public static void main(String[] args) {
-        int[] arr = { 1, 4, 2, 6, 3, 2, 6 };
+        int[] arr = { 1, 4, 2, 5, 3, 2, 6 };
         repeatingElement(arr);
     }
 
     static void repeatingElement(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for(int j =i+1; j<arr.length; j++) {
-                if (arr[i]==arr[j]) {
-                    System.out.println("First repeated element : "+arr[i]);
-                    return;
-                }
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr) {
+            if (set.contains(num)) {
+                System.out.println("First repeated element : " + num);
+                return;
             }
+            set.add(num);
         }
-        System.out.println("No repeated element in array.");
+        System.out.println("No repeating element found");
     }
 }
