2c054ed5e023ccb4f95067748d63523677722c78
Suraj Aglave <aglavesuraj96@gmail.com>
2026-03-03 23:45:42 +0530

    -

---
+++ String/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        String str = "racecar";
+        String result = removeDuplicate(str);
+        System.out.println(result);
+    }
+
+    static String removeDuplicate(String str) {
+        Set<Character> set = new LinkedHashSet<>();
+        for (char ch: str.toCharArray()) {
+            set.add(ch);
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : set) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
