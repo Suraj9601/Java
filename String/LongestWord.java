b93c6d456c2458a76c82bfb4cb510e8a8c04dbb6
Suraj Aglave <aglavesuraj96@gmail.com>
2026-03-04 17:38:49 +0530

    -

--- String/LongestWord.java
+++ String/LongestWord.java
@@ -1,16 +1,18 @@
 public class LongestWord {
     public static void main(String[] args) {
-        String str = "My name is Suraj";
-        
+        String str = "My name is Suraj Aglave";
         System.out.println(findLongestWord(str));
     }
 
-    static int findLongestWord(String str) {
-        String[] s = str.split(" ");
-        int len = 0;
-        for (int i = 0; i < s.length; i++) {
-            
+    static String findLongestWord(String str) {
+        String[] words = str.split(" ");
+        String longest = "";
+
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
         }
-        return len;
+        return longest;
     }
 }
--- String/StringTitleCase.java
+++ String/StringTitleCase.java
@@ -1,9 +1,13 @@
 public class StringTitleCase {
     public static void main(String[] args) {
-        
+        String str = "my";
+        titleCase(str);
     }
 
     static void titleCase(String str) {
+        for (String word : str.split(str)) {
+        char ch = word.charAt(0);
         
+        }
     }
 }
