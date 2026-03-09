3a7ae3efa0bbfcb7150157cb98c01447fe6f7af5
Suraj Aglave <aglavesuraj96@gmail.com>
2026-03-09 16:48:37 +0530

    -

--- DSA/Searching/RecursiveBinarySearch.java
+++ DSA/Searching/RecursiveBinarySearch.java
@@ -1,7 +1,8 @@
 public class RecursiveBinarySearch {
     public static void main(String[] args) {
         int[] arr = { 40, 45, 50, 68, 70, 85 };
-        System.out.println(binarySearch(arr, 50, 0, arr.length - 1));
+        int target = 50;
+        System.out.println(binarySearch(arr, target, 0, arr.length - 1));
     }
 
     static int binarySearch(int[] arr, int target, int left, int right) {
