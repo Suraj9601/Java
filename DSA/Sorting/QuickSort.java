b20b4a679aa6da80e01f7bf4c1ff596da74c7b57
Suraj Aglave <aglavesuraj96@gmail.com>
2026-03-16 20:28:28 +0530

    -

---
+++ DSA/Sorting/QuickSort.java
@@ -0,0 +1,11 @@
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = { 2, 5, 3, 8, 9, 1 };
+        quickSort(arr, 0, arr.length-1);
+    }
+
+    static void quickSort(int[] arr, int st, int end) {
+        int pivot = arr[0];
+        
+    }
+}
