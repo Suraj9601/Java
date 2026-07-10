fbfec3d97ca606a45df59e7b5ad24996b9bbe907
Suraj Aglave <aglavesuraj96@gmail.com>
2026-07-10 21:05:33 +0530

    Create DeleteOperations.java

---
+++ DSA/src/com/LinkedList/DeleteOperations.java
@@ -0,0 +1,31 @@
+package com.LinkedList;
+
+class Node {
+	int val;
+	Node next;
+	
+	Node() {
+		this.next = null;
+	}
+	
+	Node(int val) {
+		this.val = val;
+	}
+	
+	void deleteFirst() {
+		
+	}
+	
+	void display() {
+		
+	}
+}
+
+public class DeleteOperations {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
