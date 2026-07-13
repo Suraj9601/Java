c2ae3f08538c114a42bb4d5f853729f4bd4a9091
Suraj Aglave <aglavesuraj96@gmail.com>
2026-07-13 15:33:59 +0530

    .

---
+++ DSA/src/com/LinkedList/Singly/ReverseLinkedList.java
@@ -0,0 +1,33 @@
+package com.LinkedList.Singly;
+
+public class ReverseLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		
+		list.insertLast(10);
+		list.insertLast(20);
+		list.insertLast(30);
+		list.insertLast(40);
+		list.insertLast(50);
+		
+		list.display();
+		reverseLL(list);
+		list.display();
+	}
+	
+	public static void reverseLL(LinkedList list) {
+		Node prevNode = null;
+		Node currNode = list.head;
+		Node nextNode;
+		
+		while(currNode != null) {
+			nextNode = currNode.next;
+			currNode.next = prevNode;
+			prevNode = currNode;
+			currNode = nextNode;
+		}
+		list.head = prevNode;
+	}
+
+}
