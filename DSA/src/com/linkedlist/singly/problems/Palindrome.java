4314a37886fa1ca3d022f6ae09fdf06b31a9934c
Suraj Aglave <aglavesuraj96@gmail.com>
2026-07-20 16:29:53 +0530

    .

--- DSA/src/com/linkedlist/singly/problems/Palindrome.java
+++ DSA/src/com/linkedlist/singly/problems/Palindrome.java
@@ -1,10 +1,54 @@
 package com.linkedlist.singly.problems;
+import com.linkedlist.singly.*;
 
 public class Palindrome {
 
 	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
 		
+		list.insertLast(1);
+		list.insertLast(2);
+		list.insertLast(2);
+		list.insertLast(1);
+		
+		list.display();
+		
+		System.out.println("LL is Palindrome : "+ isPalindrome(list.head));
 
+	}
+	
+	public static boolean isPalindrome(Node head) {
+		if(head == null || head.next == null) return true;
+		
+		Node slow = head, fast = head;
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		Node midNode = slow;
+		
+		Node prev = null;
+		Node curr = midNode;
+		Node next;
+		
+		while(curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		
+		Node right = prev;
+		Node left = head;
+		
+		while(right != null) {
+			if(left.data != right.data ) {
+				return false;
+			}
+			left = left.next;
+			right = right.next;
+		}
+		return true;
 	}
 
 }
