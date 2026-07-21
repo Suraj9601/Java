edff0855c6b2c1bb5448f9d8b5762cc608f37713
Suraj Aglave <aglavesuraj96@gmail.com>
2026-07-21 12:10:31 +0530

    .

--- DSA/src/com/linkedlist/singly/problems/MergeSortLinkedList.java
+++ DSA/src/com/linkedlist/singly/problems/MergeSortLinkedList.java
@@ -3,7 +3,7 @@ package com.linkedlist.singly.problems;
 public class MergeSortLinkedList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- DSA/src/com/linkedlist/singly/problems/ReorderLinkedList.java
+++ DSA/src/com/linkedlist/singly/problems/ReorderLinkedList.java
@@ -1,10 +1,44 @@
 package com.linkedlist.singly.problems;
+import com.linkedlist.singly.*;
+
+// Zigzag / Reorder Linked List
 
 public class ReorderLinkedList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	
+	public static void reOrder(Node head) {
+		Node slow = head;
+		Node fast = head.next;
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		Node mid = slow;
+		
+		
+		
+		Node curr = mid.next;
+		mid.next = null;
+		
+		Node prev = null;
+		Node next;
+		
+		while(curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		Node left = prev;
+		Node right = head;
+		
+		while(left != null && right != null) {
+			
+		}
 	}
 
 }
